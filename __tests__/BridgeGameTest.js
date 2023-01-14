1c7c8128ac7da04b26b354a3ebff3bf37fbf173a
chiman <rlaclghks123@naver.com>
2023-01-14 16:45:02 +0900

    Test: test코드 beforeEach를 사용해 중복제거

--- __tests__/BridgeGameTest.js
+++ __tests__/BridgeGameTest.js
@@ -2,16 +2,19 @@ const BridgeGame = require('../src/model/BridgeGame');
 const Bridge = require('../src/model/Bridge');
 
 describe('getGameStatus Test', () => {
-  test('gameStatus 기본값 Test', () => {
-    const bridge = new Bridge(3);
-    const bridgeGame = new BridgeGame(bridge);
+  let bridge;
+  let bridgeGame;
 
+  beforeEach(() => {
+    bridge = new Bridge(3);
+    bridgeGame = new BridgeGame(bridge);
+  });
+
+  test('gameStatus 기본값 Test', () => {
     expect(bridgeGame.getGameStatus()).toEqual('playing');
   });
 
   test('gameStatus Retry 했을때 다시 기본값이 되는지 Test', () => {
-    const bridge = new Bridge(3);
-    const bridgeGame = new BridgeGame(bridge);
     bridgeGame.move('D');
     bridgeGame.retry();
     expect(bridgeGame.getGameStatus()).toEqual('playing');
